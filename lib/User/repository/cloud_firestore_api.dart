a113fe47883b5b88892bb19e1c6be6d362268754
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-09-06 03:32:46 +0200

    Add back button to AppBar

--- lib/User/repository/cloud_firestore_api.dart
+++ lib/User/repository/cloud_firestore_api.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_firebase_demo/User/model/user.dart';
 
-class CloudFirestoreApi {
+class CloudFirestoreAPI {
   // Collections
   final String USERS = "users";
 
@@ -10,6 +10,15 @@ class CloudFirestoreApi {
   final Firestore _db = Firestore.instance;
 
   void updateUserData(User user) async {
+    // The last user connect --btn login
     DocumentReference ref = _db.collection(USERS).document(user.uid);
+
+    return ref.setData({
+      'uid': user.uid,
+      'name': user.name,
+      'email': user.email,
+      'photoUrl': user.photoUrl,
+      'lastSignIn': DateTime.now(),
+    }, merge: true);
   }
 }
