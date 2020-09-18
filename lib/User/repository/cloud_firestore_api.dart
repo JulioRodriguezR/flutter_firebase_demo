669301b268399aa21330a5aef31ef3fa5b35c079
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-09-18 14:31:15 +0200

    Send data to firestore of firebase

--- lib/User/repository/cloud_firestore_api.dart
+++ lib/User/repository/cloud_firestore_api.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase_demo/Tabs/model/place.dart';
 import 'package:flutter_firebase_demo/User/model/user.dart';
 
 class CloudFirestoreAPI {
   // Collections
   final String USERS = "users";
-
+  final String PLACES = "places";
   // Bring user entity and make it available to be able to insert in the db
   final Firestore _db = Firestore.instance;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   void updateUserData(User user) async {
     // The last user connect --btn login
     DocumentReference ref = _db.collection(USERS).document(user.uid);
 
-    return ref.setData({
+    return await ref.setData({
       'uid': user.uid,
       'name': user.name,
       'email': user.email,
       'photoUrl': user.photoUrl,
       'lastSignIn': DateTime.now(),
     }, merge: true);
+  }
+
+  Future<void> updatePlaceDate(Place place) async {
+    CollectionReference refPlaces = _db.collection(PLACES);
+
+    await _auth.currentUser().then((FirebaseUser user) {
+      // Unique and incremental identifier
+      refPlaces.add({
+        'name': place.name,
+        'description': place.description,
+        'userOwner': "$USERS/${user.uid}", // reference
+      });
+    });
   }
 }
