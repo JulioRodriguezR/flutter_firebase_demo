3e0f70795fa1300f856f0db574c3d9c5c1498c55
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-09-22 13:56:13 +0200

    Clean code

--- lib/User/repository/firebase_auth.dart
+++ lib/User/repository/firebase_auth.dart
@@ -4,12 +4,11 @@ import 'package:google_sign_in/google_sign_in.dart';
 class FirebaseAuthAPI {
   // Register firebase
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
   // Initial session on Google
-  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<FirebaseUser> sigIn() async {
-    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
     GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
 
     FirebaseUser user = await _auth.signInWithCredential(
@@ -24,6 +23,6 @@ class FirebaseAuthAPI {
 
   signOut() async {
     await _auth.signOut().then((onValue) => print("sign out"));
-    googleSignIn.signOut();
+    _googleSignIn.signOut();
   }
 }
